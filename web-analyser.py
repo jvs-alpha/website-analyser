05f2bd54b2e6de9911a0c92f0bf2d421aad32896
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-03-27 00:37:32 +0530

    added changes to data

--- web-analyser.py
+++ web-analyser.py
@@ -13,6 +13,8 @@ parser.add_argument("url",type=str,help="This is the URl of the WebSite")
 argv = parser.parse_args()
 url = argv.url
 
+report = ""
+
 # Validating the url
 check = 0
 if(validators.url(url)):
@@ -23,6 +25,20 @@ else:
 if check == 1:
     response = requests.get(url)
     parsed_data = BeautifulSoup(response.text,"html.parser")
-    print(parsed_data.find_all("form"))
+
+    forms = parsed_data.find_all("form")
+
+    comments = parsed_data.find_all(string=lambda text:isinstance(text,Comment))
+
+    for form in forms:
+        if (form.get("action").find("https") < 0) and (urlparse(url).scheme != "https"):
+            form_is_secure = False
+            report += "Form Issue: Insecure Form actio " + form.get("action") + " Found in WebSite \n"
+        else:
+            form_is_secure = True
+    for comment in comments:
+        if comment.find("key: ") > -1:
+            report += "Comment Issue: Key is found in the html\n"
 else:
     print("Error")
+print(report)
